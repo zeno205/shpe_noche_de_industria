1a468d56533d2e85a6ed7890ac74698db1d1ba17
zeno205 <71925259+zeno205@users.noreply.github.com>
2024-09-15 22:37:55 -0400

    feat: minor adjustments to participating companies to fit the new layout

--- src/index.jsx
+++ src/index.jsx
@@ -32,8 +32,8 @@ export function App() {
 				</div>
 			</div>
 
-			<div className="container mx-auto py-8 md:py-9 px-4 relative">
-				<h1 className="text-3xl lg:text-4xl font-bold text-center text-[#003087] pb-8 md:pb-10">
+			<div className="container mx-auto py-16 px-4 relative">
+				<h1 className="text-4xl lg:text-5xl font-bold text-center text-[#003087] pb-12 drop-shadow-md">
 					Participating Companies
 				</h1>
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
